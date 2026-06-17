745ff2e372b99398df81af2008ff4424173d1b83
Fabiana Cecin <fabiana@waku.org>
2026-06-17 12:11:46 -0300

    feat: QUIC transport for simulator nodes
    * QUIC_SUPPORT env (default on) adds --quic-support
    * pass QUIC_SUPPORT via compose + wakusim.env
    * fix rest-traffic hostnames for Compose v2

--- run_bootstrap.sh
+++ run_bootstrap.sh
@@ -4,6 +4,8 @@ IP=$(ip a | grep "inet " | grep -Fv 127.0.0.1 | sed 's/.*inet \([^/]*\).*/\1/')
 
 echo "I am a bootstrap node"
 
+QUIC_ARGS=""
+[ "${QUIC_SUPPORT:-true}" = "true" ] && QUIC_ARGS="--quic-support=true"
 exec /usr/bin/wakunode\
       --relay=false\
       --rest=true\
@@ -17,4 +19,4 @@ exec /usr/bin/wakunode\
       --metrics-server-address=0.0.0.0\
       --nodekey=30348dd51465150e04a5d9d932c72864c8967f806cce60b5d26afeca1e77eb68\
       --nat=extip:${IP}\
-      --cluster-id=66
+      --cluster-id=66 $QUIC_ARGS
